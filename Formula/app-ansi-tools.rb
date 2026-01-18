cfd66b12c24b4a300ac9ae4aeb73019881d4fda9
Kazumasa Utashiro <kaz@utashiro.com>
2026-01-19 00:06:29 +0900

    Add app-ansi-tools meta-package

---
+++ Formula/app-ansi-tools.rb
@@ -0,0 +1,25 @@
+class AppAnsiTools < Formula
+  desc "ANSI terminal sequence aware text processing tools"
+  homepage "https://github.com/tecolicom"
+  url "https://cpan.metacpan.org/authors/id/U/UT/UTASHIRO/App-ansicolumn-1.50.tar.gz"
+  sha256 "4696cd85d8d41fc2c1246b071d138e636d4766e0eb0a112a8273bd7d9af5c90f"
+  license any_of: ["Artistic-1.0-Perl", "GPL-1.0-or-later"]
+  version "1.0"
+
+  depends_on "tecolicom/tap/app-ansicolumn"
+  depends_on "tecolicom/tap/app-ansifold"
+  depends_on "tecolicom/tap/app-ansiecho"
+
+  def install
+    (prefix/"README").write <<~EOS
+      This is a meta-package that installs all ANSI tools:
+      - ansicolumn, ansicolrm, ansicut (from app-ansicolumn)
+      - ansifold (from app-ansifold)
+      - ansiecho, ansiprintf (from app-ansiecho)
+    EOS
+  end
+
+  test do
+    system Formula["app-ansicolumn"].bin/"ansicolumn", "--version"
+  end
+end
